5d9886567d67f16b20efa58eee766929488dde0e
Senti0027 <5b0g0027@stust.edu.tw>
2024-05-07 21:45:46 +0800

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MySlider from './MySlider';
 
 function App() {
   return (
@@ -13,4 +14,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;//交給index.js
