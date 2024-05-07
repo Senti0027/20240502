fe200d05b6f86e6b2a489c322b2294a3cd934577
Senti0027 <5b0g0027@stust.edu.tw>
2024-05-07 21:08:16 +0800

    first

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Javascript程式設計，使用react</h1>
+      <h2>第一個元件</h2>
+      <span>R: </span><MySlider/>
+      <span>G: </span><MySlider/>
+      <span>B: </span><MySlider/>
     </div>
   );
 }
