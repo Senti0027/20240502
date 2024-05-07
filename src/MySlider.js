3c060010464d9c927977285cc22c1b930f2ae37f
Senti0027 <5b0g0027@stust.edu.tw>
2024-05-07 22:41:33 +0800

    second

--- src/MySlider.js
+++ src/MySlider.js
@@ -1,22 +1,16 @@
-import {useState}from "react";
+import { useState } from "react";//紀錄滑桿的值
 function MySlider() {
-    const [value, setValue] = useState(128);
-    const handleChange = (e) => {
-      setValue(e.target.value);
-    };
-    return (
-      <div>
-        <input
-          type="range"
-          width = "200"
-          min="0"
-          max="255"
-          value={value}
-          onChange={handleChange}
-        />
-        <span>{value}</span>
-      </div>
+  const [value, setValue] = useState(128);
+  const handChange = (e) => { 
+    setValue(e.target.value); 
+  };
+  return (
+    <div>
+      <input type="range" width="200" min="0" max="255" value={value} onChange={handChange} />
+    <span>{value}</span>
+    </div>
     );
-  }
-  
+
+}
+
 export default MySlider;
