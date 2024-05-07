840f2aac78560252930df49ffbe373ee56bf26e3
Senti0027 <5b0g0027@stust.edu.tw>
2024-05-07 23:05:06 +0800

    time

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Javascript程式設計，使用react</h1>
+      <h2>5B0G0027邱品毓</h2>
       <h2>第一個元件</h2>
       <span>R: </span><MySlider/>
       <span>G: </span><MySlider/>
